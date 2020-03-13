21a622aef86fb73f323c4db0ac4ff76fc1a383e1
Hobson Lane <hobs@totalgood.com>
2020-03-12 22:36:14 -0700

    new build.sh

--- build.sh
+++ build.sh
@@ -9,12 +9,14 @@ docker-compose down -v --remove-orphans
 if [ "$1" == 'prod' ]
 then
     echo "Building !PROD! docker image ..."
+    docker volume rm -f midata
     docker-compose -f docker-compose.prod.yml up -d --build
     docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
     echo "Starting PROD containers for the webapp at http://localhost/ ..."
     docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
 else
     echo "Building development docker image ..."
+    docker volume rm -f midata
     docker-compose -f docker-compose.yml up -d --build
     docker-compose -f docker-compose.yml exec web python manage.py migrate --no-input
     echo "Starting development containers for the webapp at http://localhost:8000/ ..."
