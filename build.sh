938f1535a200171abb7c70ddb5247d18be1ffb40
hobs <hobs+github@totalgood.com>
2020-03-12 22:42:57 -0700

    revert to /tmp/midata and not ro

--- build.sh
+++ build.sh
@@ -2,23 +2,26 @@
 set -e
 
 echo "STOPPING ALL CONTAINERS!!!"
-
 docker-compose down -v --remove-orphans
-
+echo
 
 if [ "$1" == 'prod' ]
 then
     echo "removing 'midata' docker volume"
     docker volume rm -f midata
     echo
+
     echo "Building PROD docker image ..."
     docker-compose -f docker-compose.prod.yml up -d --build
     echo
+
     echo "Migrating PROD db ..."
     docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
     echo
+
     echo "Starting PROD containers for the webapp at http://localhost/ ..."
     docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+    echo
 else
     echo "Building development docker image ..."
     docker volume rm -f midata
