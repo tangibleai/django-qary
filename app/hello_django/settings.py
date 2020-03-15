4aca209a403b58f1ab16325b501ecaf94377c5d1
Hobson Lane <hobs@totalgood.com>
2020-03-14 17:24:51 -0700

    allowed hosts fixed

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -25,12 +25,11 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = 'totalgood.org gpu.totalgood.org www.totalgood.org localhost 127.0.0.1 [::1]'.split()
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
