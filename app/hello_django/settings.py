108c0516d579022108610976c79f927eab821d83
Hobson Lane <gitlab@totalgood.com>
2020-03-28 21:55:04 -0700

    add back Dockerfile and settings.py but not .env

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -30,9 +30,8 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 
 # Application definition
