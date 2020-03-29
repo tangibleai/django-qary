5d9e9d9417d98b73c4de738d4ba85b4bf2351711
Hobson Lane <gitlab@totalgood.com>
2020-03-28 22:11:10 -0700

    checkout build script from develop too for use on server envy

--- build.sh
+++ build.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 set -e
 
-echo "STOPPING ALL CONTAINERS!!!"
-docker-compose down -v --remove-orphans
-echo
-
+echo ''
 if [ "$1" == 'prod' ]
 then
-    echo "removing 'midata' docker volume"
-    docker volume rm -f midata
-    echo
+    echo "STOPPING ALL CONTAINERS!!!"
+    docker-compose down -v --remove-orphans
 
-    echo "Building PROD docker image ..."
+    echo ''
+    echo "Building !PROD! docker image ..."
     docker-compose -f docker-compose.prod.yml up -d --build
-    echo
-
-    echo "Migrating PROD db ..."
     docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
-    echo
 
+    echo ''
     echo "Starting PROD containers for the webapp at http://localhost/ ..."
     docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
-    echo
+elif [ "$1" == 'dev' ]
+then
+    echo "STOPPING ALL CONTAINERS!!!"
+    docker-compose down -v --remove-orphans
 
-    # docker run -v /midata:/home/app/web/midata:ro web # command_to_run
-else
+    echo ''
     echo "Building development docker image ..."
-    docker volume rm -f midata
     docker-compose -f docker-compose.yml up -d --build
     docker-compose -f docker-compose.yml exec web python manage.py migrate --no-input
+
+    echo ''
     echo "Starting development containers for the webapp at http://localhost:8000/ ..."
     docker-compose -f docker-compose.yml exec web python manage.py collectstatic --no-input --clear
+elif [ "$1" == 'stop' ]
+then
+    echo "STOPPING ALL CONTAINERS!!!"
+    docker-compose down -v --remove-orphans
 fi
