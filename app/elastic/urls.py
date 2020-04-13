4685db9b195502cc87d54207b8fc7430ea1cd4ed
hobs <hobs+github@totalgood.com>
2020-04-12 20:58:24 -0700

    refactor elastic -> elastic_app

--- app/elastic/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from elastic import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.test_connection, name = 'connection'),
-    path('search/', views.search_index, name = "search"),
-]
