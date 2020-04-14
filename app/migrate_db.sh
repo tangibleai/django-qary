96199ad86b9f2ca93207e686d47726a1b93ab446
hobs <hobs+github@totalgood.com>
2020-04-14 14:41:24 -0700

    USER app from the beginning

--- app/migrate_db.sh
+++ app/migrate_db.sh
@@ -15,7 +15,7 @@ echo "Running migrate_db.sh ..."
 ls -hal /home/app/web/staticfiles/admin/js
 
 python manage.py flush --no-input
-python manage.py migrate
+python manage.py migrate upload
 python manage.py collectstatic --no-input --clear
 
 exec "$@"
