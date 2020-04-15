22b6a2292658771380965c3618fef2d9b3bd4e23
hobs <hobs+github@totalgood.com>
2020-04-15 14:13:51 -0700

    formatting

--- app/elastic_app/es_index.py
+++ app/elastic_app/es_index.py
@@ -1,6 +1,6 @@
 import wikipediaapi
 from slugify import slugify
-from elasticsearch import Elasticsearch 
+from elasticsearch import Elasticsearch
 from search import search
 
 try:
@@ -12,105 +12,106 @@ wiki_wiki = wikipediaapi.Wikipedia('en')
 
 mapping = {
     "properties": {
-        
-            "text": {
-                "type": "nested",
-                "properties":{
-                    "section_num": {"type":"integer"},
-                    "section_title": {"type":"text"},
-                    "section_content": {"type":"text"}
-                }
-            },
-        
-            "references": {
-                "type": "nested",
-                "properties":{
-                    "section_num": {"type":"integer"},
-                    "section_title": {"type":"text"},
-                    "section_content": {"type":"text"}
-                }
-            },
-        
-            "title": {
-                "type": "text"
-            },
-        
-            "source": {
-                "type": "text"
-            },
-        
-            "page_id": {
-                "type": "long"
-            },
-            
-        }
+
+        "text": {
+            "type": "nested",
+            "properties": {
+                    "section_num": {"type": "integer"},
+                    "section_title": {"type": "text"},
+                    "section_content": {"type": "text"}
+            }
+        },
+
+        "references": {
+            "type": "nested",
+            "properties": {
+                    "section_num": {"type": "integer"},
+                    "section_title": {"type": "text"},
+                    "section_content": {"type": "text"}
+            }
+        },
+
+        "title": {
+            "type": "text"
+        },
+
+        "source": {
+            "type": "text"
+        },
+
+        "page_id": {
+            "type": "long"
+        },
+
     }
+}
+
 
 class Document:
-    
+
     def __init__(self):
         self.title = ''
         self.page_id = None
         self.source = ''
         self.text = ''
-        
+
     def __if_exists(self, page_id, index=""):
         '''
         Check if the article already exists in the database
         with a goal to avoid duplication
         '''
-        
-        return client.search(index=index, 
-                             body={"query": 
-                                   {"match": 
+
+        return client.search(index=index,
+                             body={"query":
+                                   {"match":
                                     {"page_id": page_id}
-                                   }})['hits']['total']['value']
-        
+                                    }})['hits']['total']['value']
+
     def insert(self, title, page_id, url, text, references, index):
         ''' Add a new document to the index'''
-        
-        self.title=title
-        self.page_id=page_id
-        self.source=url
-        self.text=text
+
+        self.title = title
+        self.page_id = page_id
+        self.source = url
+        self.text = text
         self.references = references
         self.body = {'title': self.title,
-            'page_id': self.page_id,
-            'source':self.source,
-            'text': self.text,
-            'references':self.references}
-        
+                     'page_id': self.page_id,
+                     'source': self.source,
+                     'text': self.text,
+                     'references': self.references}
+
         if self.__if_exists(page_id) == 0:
-        
+
             try:
                 client.index(index=index, body=self.body)
                 print(f'Successfully added document {self.title} to index {index}.')
             except Exception as error:
                 print(f"Error writing document {page_id}: {error}")
-                
+
         else:
             print(f"Article {self.title} is already in the database")
 
 
 def parse_article(article):
-    ''' 
+    '''
     Parse full wikipedia article into sections:
     - content sections (summary, History, Applications, etc.)
     - reference section (References, Bibliography, See Also, Notes, etc)
     '''
-    
+
     text = article.text
     # get section titles for the existing sections
     section_titles = [sec.title for sec in article.sections]
-    
-    # initiate the sections dictionary with a summary (0th section) 
+
+    # initiate the sections dictionary with a summary (0th section)
     sections = [{'section_num': 0,
-                'section_title': "Summary",
-                'section_content': article.summary}]
-    
+                 'section_title': "Summary",
+                 'section_content': article.summary}]
+
     for i, title in enumerate(section_titles[::-1]):
 
-        num = len(section_titles)-i
+        num = len(section_titles) - i
         if len(text.split(f"\n\n{title}")) == 2:
             section_dict = {"section_num": num,
                             "section_title": title,
@@ -120,9 +121,9 @@ def parse_article(article):
         else:
             print(f"Skipping section {title} (empty)S.")
             pass
-            
-        
+
     return sections
+
 
 def get_references(mylist):
     '''
@@ -130,44 +131,40 @@ def get_references(mylist):
     '''
     reference_list = []
     content_list = []
-    
+
     for d in mylist:
         if d['section_title'].lower() in ' '.join(['see also references external links bibliography notes']):
             reference_list.append(d)
         else:
             content_list.append(d)
-            
-    return (content_list, reference_list)  
+
+    return (content_list, reference_list)
 
 
 def search_insert_wiki(category, mapping):
-    
-    if type(category) is not list: category = [ category ]
+
+    if type(category) is not list:
+        category = [category]
 
     wiki_wiki = wikipediaapi.Wikipedia('en')
-    
+
     for c in category:
-        
         try:
-                    
             '''Create and empty index with predefined data structure'''
-            client.indices.create(index=slugify(c), body={"mappings":mapping})
+            client.indices.create(index=slugify(c), body={"mappings": mapping})
             print(f'New index {slugify(c)} has been created')
-            
-            '''Access the list of wikipedia articles in category c'''
+
+            '''Access the wikipedia article that lists wikipedia articles and urls in the category `c`'''
             cat = wiki_wiki.page(f"Category:{c}")
-            
+
             ''' Parse and add articles in the category to database'''
             for key in cat.categorymembers.keys():
                 page = wiki_wiki.page(key)
-
-                if not "Category:" in page.title:
-
+                if "Category:" not in page.title:
                     text = parse_article(page)
                     content, references = get_references(text)
                     doc = Document()
                     doc.insert(page.title, page.pageid, page.fullurl, content, references, index=slugify(c))
-
 
         except Exception as error:
             print(f"The following exception occured while trying to create index '{slugify(c)}': ", error)
@@ -182,12 +179,13 @@ def test_search(statement):
         print(doc['_source']['source'])
         print("----------------------")
 
+
 def test_index(category):
     search_insert_wiki(category, mapping=mapping)
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
+
     # test_index('American science fiction television series')
     test_search("when barack obama was inaugurated?")
 
@@ -198,10 +196,10 @@ if __name__=="__main__":
     #             'American science fiction television series',
     #             'Science fiction television',
     #             'Natural language processing',
-    #             'American comics writers', 
+    #             'American comics writers',
     #             'Presidents of the United States',
     #             'Coronaviridae',
     #             'Pandemics'
     #             ]
-    
+
     # search_insert_wiki(categories, mapping)
