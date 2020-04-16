88c0f4785b842f43b5f1ce05567d81231a67fb02
hobs <hobs+github@totalgood.com>
2020-04-15 18:41:02 -0700

    clean up script

--- app/index_wikipedia.py
+++ app/index_wikipedia.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """ Add documents from en.wikipedia.org to an index called "wikipedia" """
+import logging
 
 from elastic_app.constants import ES_CATEGORIES, ES_SCHEMA
 from elastic_app.es_search import search
+from elastic_app.es_index import search_insert_wiki
 
+log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     hits = search("When was Barack Obama inaugurated?").get('hits', {}).get('hits', [])
