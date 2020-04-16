8f44873aa805369894d42aa327a8b0b895e64e51
hobs <hobs+github@totalgood.com>
2020-04-15 18:52:54 -0700

    build runs index_wikipedia

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,14 +10,14 @@ then
     echo "STOPPING ALL CONTAINERS!!!"
     docker-compose -f docker-compose.prod.yml down -v --remove-orphans
 
-    echo ''
+    echo ""
     echo "Bringing up docker-compose.prod.yml docker images ..."
     docker-compose -f docker-compose.prod.yml up -d --build
 
     echo "Migrating DB in PROD containers with exec web python manage.py migrate (without --no-input)"
     docker-compose  -f docker-compose.prod.yml exec --user app web python manage.py migrate
 
-    echo ''
+    echo ""
     echo "Collecting static in PROD containers for the webapp with exec web python manage.py collect static ..."
     docker-compose  -f docker-compose.prod.yml exec --user app web python manage.py collectstatic --no-input  # --clear
 
@@ -31,13 +31,13 @@ then
     echo "STOPPING ALL CONTAINERS!!!"
     docker-compose -f docker-compose.dev.yml down -v --remove-orphans
 
-    echo ''
+    echo ""
     echo "Building development docker image with nginx path=$MIDATA_HOST_PATH ..."
     docker-compose -f docker-compose.dev.yml up -d --build
     echo "Running exec web python manage.py migrate (without --no-input)"
     docker-compose -f docker-compose.dev.yml exec web python manage.py migrate
 
-    echo ''
+    echo ""
     echo "Starting development containers for the webapp at http://localhost:8000/ ..."
     docker-compose -f docker-compose.dev.yml exec web python manage.py collectstatic --no-input --clear
 elif [ "$1" == 'stop' ]
