9a5a4d9b88a9b1957c51de7f351100d956aa9eb0
hobs <hobs+github@totalgood.com>
2020-04-19 00:02:54 -0700

    mkdir

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -12,12 +12,6 @@ then
     echo "PostgreSQL started"
 fi
 
-mkdir -p $HOME/midata/private/static_volume
-mkdir -p $HOME/midata/private/media_volume
-mkdir -p $HOME/midata/private/esdata
-mkdir -p $HOME/midata/private/postgres_data
-chown -R 1000:1000 $HOME/midata/private
-
 # prevents access denied error during collect static while creating static_volume/staticfiels/admin/ dir:
 chmod -R a+w $HOME/midata/private/static_volume
 echo "Running entrypoint.prod.sh ..."
