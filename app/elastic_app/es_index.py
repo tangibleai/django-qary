b3f9a8fd72d8662d9dbdc95ee7d743f7c996fbb6
hobs <hobs+github@totalgood.com>
2020-04-20 15:05:54 -0700

    Nonworking WikiDocument class that inherits WikipediaPage class from wikipediaapi

--- app/elastic_app/es_index.py
+++ app/elastic_app/es_index.py
@@ -56,6 +56,43 @@ from .constants import ES_SCHEMA, ES_CATEGORIES, ES_INDEX, ES_HOST, ES_PORT
 log = logging.getLogger(__name__)
 
 
+class WikiDocument(wikipediaapi.WikipediaPage):
+    """ WIP: WikipediaPage wrapper with parse_article() method to produce Elasticsearch-indexable object """
+
+    def get_sections(self):
+        pass
+
+    def parse_article(self):
+        ''' Parse full wikipedia article into sections:
+        - content sections (summary, History, Applications, etc.)
+        - reference section (References, Bibliography, See Also, Notes, etc)
+        '''
+
+        text = self.text
+        # get section titles for the existing sections
+        section_titles = [sec.title for sec in self.sections]
+
+        # initiate the sections dictionary with a summary (0th section)
+        sections = [{'section_num': 0,
+                     'section_title': "Summary",
+                     'section_content': self.summary}]
+
+        for i, title in enumerate(section_titles[::-1]):
+
+            num = len(section_titles) - i
+            if len(text.split(f"\n\n{title}")) == 2:
+                section_dict = {"section_num": num,
+                                "section_title": title,
+                                "section_content": text.split(f"\n\n{title}")[-1]}
+                sections.append(section_dict)
+                text = text.split(f"\n\n{title}")[0]
+            else:
+                log.info(f"Skipping section {title} (empty)S.")
+                pass
+
+        return sections
+
+
 class Document:
 
     def __init__(self, title='', page_id=None, source='', text='',
