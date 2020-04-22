c71a9cf57cc1d7e3fbf6fbc27e36ea27891a5250
hobs <hobs+github@totalgood.com>
2020-04-22 15:18:17 -0700

    tasks.py template for celery

---
+++ app/elastic_app/tasks.py
@@ -0,0 +1,20 @@
+""" Celery tasks
+
+References:
+- https://raw.githubusercontent.com/ruddra/docker-django/master/src/myapp/tasks.py
+
+"""
+from __future__ import absolute_import, unicode_literals
+import logging
+
+from django.conf import settings
+from mydjango.celery import app
+
+logger = logging.getLogger("celery")
+
+
+@app.task
+def show_hello_world():
+    logger.info("-" * 25)
+    logger.info("Printing Hello from Celery")
+    logger.info("-" * 25)
