9aed91ee596bf02275445089d28f6cf2e62df605
hobs <hobs+github@totalgood.com>
2020-04-23 13:30:15 -0700

    obsfucation (obsification) of upload view

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -1,5 +1,6 @@
 echo '
 [remote "all"]
-    url=git@github.com:totalgood/django-qary.git
     url=git@gitlab.com:tangibleai/django-qary.git
+    url=git@github.com:totalgood/django-qary.git
 ' >> .git/config
+#  url=git@github.com:nlpia/django-qary.git
