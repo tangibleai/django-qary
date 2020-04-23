fcf8afb7b0848c16c181db4fab4ed6bced535b72
hobs <hobs+github@totalgood.com>
2020-04-23 16:24:05 -0700

    10s timeout on find_answers

--- app/elastic_app/es_search.py
+++ app/elastic_app/es_search.py
@@ -86,31 +86,36 @@ def find_snippets(statement, index=ES_INDEX, host=ES_HOST, port=ES_PORT):
 def find_answers(statement, index=ES_INDEX, host=ES_HOST, port=ES_PORT):
     """ Query Elasticsearch using statement as query string and format results as list of 8-tuples """
     global QABOT
+    t0 = time.time()
     query_results = search(text=statement, index=index, host=host, port=port)
     results = []
     for i, doc in enumerate(query_results.get('hits', query_results).get('hits', query_results)):
-        for highlight in doc.get('inner_hits', doc).get('text', doc).get('hits', doc).get('hits', {}):
-            snippet = ' '.join(highlight.get('highlight', {}).get('text.section_content', []))
-            try:
-                log.warning(f'qary.__version__: {qary.__version__}')
-                log.warning(f'QABOT: {QABOT}')
-                log.warning(f'QABOT.__class__: {QABOT.__class__}')
-                log.warning(f'Bot.__bases__: {QABOT.__class__.__bases__}')
-                log.warning(f'QABOT.context: {QABOT.context}')
-                QABOT.reset_context(context={'doc': {'text': snippet}})
-                bot_reply = QABOT.reply(statement)
-            except Exception as e:
-                log.warning(f'reset_context or .reply failed: {e}')
+        if i < 5 and time.time() - t0 < 10.:
+            for j, highlight in enumerate(doc.get('inner_hits', doc).get('text', doc).get('hits', doc).get('hits', {})):
+                snippet = ' '.join(highlight.get('highlight', {}).get('text.section_content', []))
                 bot_reply = ''
-            hit = dict(
-                title=doc['_source']['title'],
-                score=doc['_score'],
-                source=doc['_source']['source'],
-                snippet=snippet,
-                section_num=highlight['_source']['section_num'],
-                section_title=highlight['_source']['section_title'],
-                section_score=highlight['_score'],
-                reply=bot_reply)
-            results.append(hit)
+                if j < 3 and time.time() - t0 < 10.:
+                    try:
+                        log.warning(f'qary.__version__: {qary.__version__}')
+                        log.warning(f'QABOT: {QABOT}')
+                        log.warning(f'QABOT.__class__: {QABOT.__class__}')
+                        log.warning(f'Bot.__bases__: {QABOT.__class__.__bases__}')
+                        log.warning(f'QABOT.context: {QABOT.context}')
+                        QABOT.reset_context(context={'doc': {'text': snippet}})
+                        log.warning(f'QABOT.context after reset: {QABOT.context}')
+                        bot_reply = QABOT.reply(statement)
+                    except Exception as e:
+                        log.warning(f'reset_context or .reply failed: {e}')
+                        bot_reply = ''
+                hit = dict(
+                    title=doc['_source']['title'],
+                    score=doc['_score'],
+                    source=doc['_source']['source'],
+                    snippet=snippet,
+                    section_num=highlight['_source']['section_num'],
+                    section_title=highlight['_source']['section_title'],
+                    section_score=highlight['_score'],
+                    reply=bot_reply)
+                results.append(hit)
 
     return results
